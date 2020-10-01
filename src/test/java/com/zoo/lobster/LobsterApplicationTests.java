4e65f79aeb68776a28c28097e5bf6d57c2522310
2039180+mitten-crab <2039180+mitten-crab@user.noreply.gitee.com>
2020-10-02 00:14:50 +0800

    Initialize project framework

---
+++ src/test/java/com/zoo/lobster/LobsterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zoo.lobster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LobsterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
